7b2fa3b2c931103eb413b0c2528f2cc76e4222f0
Mahletk1 <mah.kumelachew@gmail.com>
2023-02-17 03:57:12 +0300

    overview page

--- src/Overview/index.js
+++ src/Overview/index.js
@@ -1,10 +1,30 @@
 import React,{Component} from "react";
 import {Chart} from "react-google-charts";
+import Card from "antd/es/card/Card";
+import {Avatar, Progress, Button, Space, Row, Col} from "antd";
 
 export class Overview extends Component {
     render() {
         
         return (
+
+           <> 
+           <div style={{margin:'30px'}}>
+           <Row  gutter={[16, 16]}>
+                 <Col span={6} className="pr-5" style={{marginBottom:'30px'}}>
+                 <Card >Card content</Card>
+                </Col>
+                <Col span={6} className="pr-5">
+                 <Card >Card content</Card>
+                </Col>
+                <Col span={6} className="pr-5">
+                 <Card >Card content</Card>
+                </Col>
+                <Col span={6} className="pr-5">
+                 <Card >Card content</Card>
+                </Col>
+            </Row>
+
             <Chart
                 chartType="ScatterChart"
                 data={[["Age", "Weight"], [4, 5.5], [8, 12]]}
@@ -12,6 +32,8 @@ export class Overview extends Component {
                 height="400px"
                 legendToggle
                 />
+                </div>
+                </>
           );
     }
 }
